c4c686d988ba2bc431327d516aa3add95a5f74bc
rafael <rafaelcopado30@gmail.com>
2024-02-14 18:38:55 -0800

    SeekBars in place, but cant get spinner to display options using only xml

---
+++ app/src/main/java/edu/up/faceMaker/FaceView.java
@@ -0,0 +1,13 @@
+package edu.up.faceMaker;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class FaceView extends SurfaceView {
+
+
+    public FaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
